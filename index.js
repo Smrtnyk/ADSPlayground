eb6b5997ade988b6968685ff6646f887a6ac407c
Milan Keser <killermesser@live.com>
2019-05-28 17:21:20 +0200

    Init commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+	SORTING
+ */
+
+const arr = [3, 2, 123, 76, 44, 56, 88, 24, 11, 12, 4, 32]
+
+// Bubble sort - Runtime complexity worst case is O(n^2)
+function bubbleSort(arr) {
+	// Implement bubbleSort
+	
+	 
+	// return sorted array
+}
