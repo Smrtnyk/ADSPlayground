58e8476699e369b337a1390849e0127a4047b1ab
Milan Keser <killermesser@live.com>
2019-05-28 17:29:02 +0200

    Bubble Sort

--- index.js
+++ index.js
@@ -2,12 +2,22 @@
 	SORTING
  */
 
-const arr = [3, 2, 123, 76, 44, 56, 88, 24, 11, 12, 4, 32]
+const arr = [3, 2, 123, 76, 44, 56, 88, -66, 24, 11, 12, 4, 32]
 
 // Bubble sort - Runtime complexity worst case is O(n^2)
 function bubbleSort(arr) {
 	// Implement bubbleSort
-	
-	 
+	for(let i = 0; i < arr.length; i++) {
+		for(let j = 0; j < (arr.length - i - 1); j++) {
+			if(arr[j] > arr[j + 1]) {
+				const smaller = arr[j + 1];
+				arr[j + 1] = arr[j];
+				arr[j] = smaller;
+			}
+		}
+	}
 	// return sorted array
+	return arr;
 }
+
+console.log(bubbleSort(arr))
